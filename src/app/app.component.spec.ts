334f9ffdc388d282cfcbc7ce5af0586d8e87c6a5
alce65 <alce65@hotmail.es>
2019-11-04 18:33:41 +0100

    Modificaciones iniciales

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-docout'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-docout');
+    expect(app.title).toEqual('Angular DocOut');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-docout app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular DocOut app is running!');
   });
 });
