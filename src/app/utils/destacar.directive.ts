9f51e1c18c4f20f2d7bd86676f8b98128874eea7
alce65 <alce65@hotmail.es>
2019-11-13 16:33:38 +0100

    Directivas Final

--- src/app/utils/destacar.directive.ts
+++ src/app/utils/destacar.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, ElementRef, Input } from '@angular/core';
+import { Directive, OnInit, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[docDestacar]'
@@ -6,13 +6,24 @@ import { Directive, OnInit, ElementRef, Input } from '@angular/core';
 export class DestacarDirective implements OnInit {
 
   @Input() docDestacar: string;
-  constructor(public element: ElementRef) { 
+ 
 
+  constructor(public element: ElementRef) {
   }
 
   ngOnInit() {
     console.log('Directiva aplicada');
     console.dir(this.element);
+    this.element.nativeElement.classList.add('destacar');
+    this.element.nativeElement.innerHTML += this.docDestacar.toUpperCase()
     console.log(this.docDestacar);
   }
+
+  @HostListener('click', ['$event']) onClick(params) {
+    console.log('Has hecho click');
+    console.log(params);
+    this.element.nativeElement.classList.toggle('destacar');
+  }
+
+
 }
